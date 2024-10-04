685a8dd66e307fc02ff5ffd794c74b57698e5082
aditya <aditya.pandey.241103@gmail.com>
2024-10-04 15:53:30 +0530

    Adjacency list representation in a directed graph

---
+++ Graphs/03_Adjacency_List_Directed.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int v, e;
+    cout << "Enter Number of vertices : ";
+    cin >> v;
+    cout << endl;
+
+    cout << "Enter Number of Edges : ";
+    cin >> e;
+    cout << endl;
+
+    vector<vector<int>> adj(v + 1);
+
+    cout << "Enter " << e << " " << " pairs of edges : " << endl;
+
+    for (int i = 0; i < e; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+    }
+
+    for (int i = 1; i <= v; i++)
+    {
+        cout << i << " ---> ";
+        for (int j = 0; j < adj[i].size(); j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
